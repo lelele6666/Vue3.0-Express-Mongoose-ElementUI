894a104f24abe8ee71649fd4260e9ba6e3c846b3
zhangyu <zhangyu@1124089875@qq.com>
2018-09-28 12:14:27 +0800

    add 404.vue

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Nofind from '@/views/404'
 
 Vue.use(Router)
 
@@ -9,17 +9,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '*',
+      name: 'Nofind',
+      component: () => import('@/views/404')
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    
   ]
 })
---
+++ client/src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nofind">
+        <img src="../assets/404.gif" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nofind{
+    width: 100%;
+    height:100%;
+}
+.nofind img{
+    width:100%;
+    height:100%; 
+}
+
+</style>
